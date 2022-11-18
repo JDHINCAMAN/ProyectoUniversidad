20377d89fe9685894f41079d50fd801fb050e165
JDHINCAMAN <nverdolaga2003@gmail.com>
2022-11-18 14:04:36 -0500

    Creado servicio para guardar empleados

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -3,7 +3,10 @@ package com.gestion.empleados.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,7 @@ import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpleadoControlador {
 
 	@Autowired
@@ -20,5 +24,10 @@ public class EmpleadoControlador {
 	@GetMapping("/empleados")
 	public List<Empleado> listarTodosLosEmpleados() {
 		return repositorio.findAll();
+	}
+	
+	@PostMapping("/empleados")
+	public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
+		return repositorio.save(empleado);
 	}
 }
